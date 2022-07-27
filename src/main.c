e59c7d421ad8557a4586d4431788480917204c39
Guilherme Oliveira <guilherme.061@gmail.com>
2022-07-26 21:33:17 -0700

    step 6: profit

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
 	"void main()\n"
 	"{\n"
-	"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"   FragColor = vec4(0.33f, 0.5f, 0.2f, 1.0f);\n"
 	"}\n\0";
 
 unsigned int shaderProgram;
